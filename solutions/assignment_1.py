52952182436f98cd0b98f3f9d63080d055a60146
rijojohn <rijojohn85@gmail.com>
2025-01-22 08:55:21 +0530

    ammended assignment1 to use command line agruments

--- solutions/assignment_1.py
+++ solutions/assignment_1.py
@@ -1,17 +1,20 @@
 # Assignment 1: Write a python script which will be able to accept the data on command line as input and will be able to identify and print out the datatype of the given input.
 #
 import re
+import sys
 
 
 def check_input_type():
-    input_from_user = input("Enter: ")
+    if len(sys.argv) < 2:
+        print("Please provide input")
+        return
+    input_from_user = sys.argv[1]
     # regex for float:
-    """
-    ^-? - starts with '-' with either 0 or 1 char: ie could be -1.1 or 1.1 ? - means count of 0 or 1
-    \d+ - digits - 1 or more - \ to escape d - digit + - 1 or more
-    \. - 1 '.'
-    \d+$ - 1 or more digits and ends with it.
-        """
+    # ^-? - starts with '-' with either 0 or 1 char: ie could be -1.1 or 1.1 ? - means count of 0 or 1
+    # \d+ - digits - 1 or more - \ to escape d - digit + - 1 or more
+    # \. - 1 '.'
+    # \d+$ - 1 or more digits and ends with it.
+
     float_pattern = r"^-?\d+\.\d+$"
 
     if input_from_user.isdigit():
